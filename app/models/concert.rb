ce945406bd0def0873f0cb2d3ba850dfda25d09b
Ssamael <hikokoro06@gmail.com>
2018-03-15 16:34:18 +0100

    new concert form fixed changes in concert show

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -3,6 +3,5 @@ class Concert < ActiveRecord::Base
 				presence: true,
 				length: {minimum: 2}
 	validates_presence_of :date, :place
-	validates_acceptance_of :ticket_price, :buy_ticket, :more_info
 
 end
