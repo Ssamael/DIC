28fb13dd2bbe60d1a00def470546f682390445f8
Ssamael <hikokoro06@gmail.com>
2018-05-27 19:16:22 +0200

    Create homepage tests

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,8 +3,29 @@ require "rails_helper"
 RSpec.feature "Homepage", type: :feature do
   scenario "User sees mainpage" do
     visit "/"
-
-
     expect(page).to have_text(I18n.t("concerts.list"))
+  end
+
+  let!(:concert) { FactoryBot.create(:concert) }
+
+  context 'User is log in' do
+    before(:each) do
+      login_as(user, scope: :user)
+    end
+    
+    context 'User is administrator' do
+      let(:user) { FactoryBot.create(:user, admin: true)}    
+      scenario "Admin user visit homepage" do
+        visit "/"
+        expect(page).to have_text("Destroy")
+      end
+    end
+
+    context 'User is not administrator' do
+      let(:user) {FactoryBot.create(:user) }
+      scenario "Standard user visit homepage" do
+        expect(page).not_to have_text("Destroy")
+      end
+    end
   end
 end
