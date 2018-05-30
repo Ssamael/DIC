d84b977259b6d17f13b56017c712e7231d2a1ed0
Ssamael <hikokoro06@gmail.com>
2018-05-30 18:01:32 +0200

    Add test for not logged in user

--- spec/features/join_concert_spec.rb
+++ spec/features/join_concert_spec.rb
@@ -3,19 +3,28 @@ require "rails_helper"
 RSpec.feature "Concert join", type: :feature do
   let(:user) { FactoryBot.create(:user) }
   let!(:concert) { FactoryBot.create(:concert) }
-
-  before(:each) do
-    login_as(user, scope: :user)
+  
+  context "User is not log in" do
+    scenario "User try to find join options" do
+      visit concert_path(concert)
+      expect(page).to have_text(I18n.t("concert.log2join"))
+    end
   end
+  
+  context "User is log in" do
+    before(:each) do
+      login_as(user, scope: :user)
+    end
 
-  scenario "User join to concert" do
-    # user = FactoryBot.create(:user)
+    scenario "User join to concert" do
+      # user = FactoryBot.create(:user)
 
-    visit concert_path(concert)
+      visit concert_path(concert)
 
 
-    click_link I18n.t("concert.join")
-    
-    expect(page).to have_text(I18n.t("concert.join_conf"))
-  end
+      click_link I18n.t("concert.join")
+      
+      expect(page).to have_text(I18n.t("concert.join_conf"))
+    end
+  end  
 end
